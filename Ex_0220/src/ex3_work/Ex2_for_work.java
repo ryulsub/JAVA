65406d391633110dfda422fd5c58e182c158b8d6
ryulsub <142034848+ryulsub@users.noreply.github.com>
2024-02-21 14:28:43 +0900

    Add files via upload

--- Ex_0220/src/ex3_work/Ex2_for_work.java
+++ Ex_0220/src/ex3_work/Ex2_for_work.java
@@ -9,6 +9,24 @@ public class Ex2_for_work {
 		// 결과 : 14
 		int n1 = 2;
 		int n2 = 5;
-
+		
+		int result = 0;
+		
+		//n1이 n2보다 크다면 두 변수의 값을 교환하여
+		//반드시 n1이 n2보다 작은수가 되도록 처리
+		if( n1 > n2 ) {
+			int n3 = n1;
+			n1 = n2;
+			n2 = n3;
+		}
+		
+		for(int i = n1; i <= n2; i++) {
+			
+			result += i;
+			
+		}//for
+		
+		System.out.println("결과 : " + result);
+			
 	}// main
 }
