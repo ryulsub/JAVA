1df2a91b6aa87383362d386dcfeea9ff0ede8b80
ryulsub <142034848+ryulsub@users.noreply.github.com>
2024-02-21 14:10:45 +0900

    Add files via upload

--- Ex_0220/src/ex3_work/Ex1_switch_work.java
+++ Ex_0220/src/ex3_work/Ex1_switch_work.java
@@ -1,5 +1,52 @@
 package ex3_work;
 
 public class Ex1_switch_work {
-
+	public static void main(String[] args) {
+		
+		//10 - 3 = 7
+		
+		int su1 = 10;
+		int su2 = 3;
+		String op = "-";
+		
+		
+		switch( op ) { // + - * /
+		
+//		case "-" :
+//			System.out.println("su1 - su2 :" + (su1 - su2) );
+//			break;
+//		
+//		case "+" :
+//			System.out.println("su1 + su2 :" + (su1 + su2) );
+//			break;
+//			
+//		case "*" :
+//			System.out.println("su1 * su2 :" + (su1 * su2) );
+//			break;
+		
+		
+		
+		//풀어준것
+		case "+":
+			System.out.printf("%d + %d = %d", su1, su2, su1+su2);
+			break;
+		
+		case "-":
+			System.out.printf("%d - %d = %d", su1, su2, su1-su2);
+			break;
+			
+		case "*":
+			System.out.printf("%d * %d = %d", su1, su2, su1*su2);
+			break;	
+		
+		case "/":
+			System.out.printf("%d / %d = %d", su1, su2, su1/su2);
+			break;	
+			
+			
+			
+			
+			
+	}//main
+  }
 }
