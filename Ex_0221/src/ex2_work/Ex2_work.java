e128ff6c09cbb4480294a3f4fa483547145bc54f
ryulsub <142034848+ryulsub@users.noreply.github.com>
2024-02-22 14:18:26 +0900

    Add files via upload

--- Ex_0221/src/ex2_work/Ex2_work.java
+++ Ex_0221/src/ex2_work/Ex2_work.java
@@ -1,23 +1,43 @@
-package ex1_work;
+package ex2_work;
 
 public class Ex2_work {
 	public static void main(String[] args) {
 		
-
 		//1 2 3 4 5 6 7 8 9 10
 		//2 3 4 5 6 7 8 9 10 1
 		//3 4 5 6 7 8 9 10 1 2
 		// ..........
 		//10 1 2 3 4 5 6 7 8 9 
 		
+		//내가푼것----------------------------------------------------
+//		for ( int i = 0; i <= 9; i++) {
+//			
+//			for(int j = 1; j <= 10; j++) {
+//				System.out.print(i + j + " ");
+//				
+//			}//inner
+//			
+//		}//outer
+	//---------------------------------------------------------------------	
 		
-		for ( int i = 1; i <= 10; i++) {
+		for(int i = 1; i <= 10; i++) {
+			
+			for(int j = 0 ; j < 10; j++) {
 				
-			for(int j = 0; j <= 10; j++) {
-				System.out.print(i + j + "");
+				int num = i + j;
+				
+				if( num > 10) {
+					//num = 2;
+					num -= 10;
+				}
+				
+				System.out.print(num + " ");
+				
 			}//inner
+			
 			System.out.println();
 		}//outer
+		
 		
 		
 	}//main
