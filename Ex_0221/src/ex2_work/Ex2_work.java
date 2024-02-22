9281e969877c3df25639891ba1119173a63a1d53
ryulsub <142034848+ryulsub@users.noreply.github.com>
2024-02-22 13:23:56 +0900

    Add files via upload

--- Ex_0221/src/ex2_work/Ex2_work.java
+++ Ex_0221/src/ex2_work/Ex2_work.java
@@ -1,20 +1,24 @@
-package ex2_work;
+package ex1_work;
 
 public class Ex2_work {
 	public static void main(String[] args) {
 		
+
 		//1 2 3 4 5 6 7 8 9 10
 		//2 3 4 5 6 7 8 9 10 1
 		//3 4 5 6 7 8 9 10 1 2
 		// ..........
 		//10 1 2 3 4 5 6 7 8 9 
 		
+		
 		for ( int i = 1; i <= 10; i++) {
-			
-			for(int j = 1; j <= 10; j++) {
 				
+			for(int j = 0; j <= 10; j++) {
+				System.out.print(i + j + "");
 			}//inner
-			
+			System.out.println();
 		}//outer
+		
+		
 	}//main
 }
